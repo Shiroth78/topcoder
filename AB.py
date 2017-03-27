1e2d8b2e3385b5ee3e91656e368a25db4f9f0d88
Shiroth78 <mino@ynl.t.u-tokyo.ac.jp>
2017-03-27 18:51:31 +0900

    AB in Practice Problems

--- AB.py
+++ AB.py
@@ -5,8 +5,11 @@ import sys
 class AB:
 	def createString(self, N, K):
 		print N,K
+		#dataをすべてBとして作る→Kの値は０
 		data = ["B" for i in range(N)]
 
+		#dataに対するKの値をチェック
+		#すべてのAに対して、後ろのBの数をカウントすればよい	
 		def checkK(data):
 			cnt = 0
 			for i in range(N):
@@ -16,7 +19,8 @@ class AB:
 							cnt += 1
 			return cnt
 
-		anum = 0
+		#dataのKと与えられたKの比較
+		#一致しなければ後ろの1個をAに変え、それを1個ずつ前にずらす(dataのKの値が1ずつ増える)
 		for i in range(N):
 			for j in range(N-i):
 				if checkK(data) == K:
@@ -32,6 +36,11 @@ class AB:
 
 		print ""
 		return ""
+
+'''
+memo
+実際にはNの半分以上がAになれば後はKの値は減っていくので、もう少し処理を軽くすることも出来る
+'''
 
 
 
